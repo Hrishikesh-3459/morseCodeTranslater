25300d72610868ad0409a711aabe23590bf8f26f
Hrishikesh Mulkutkar <51927760+Hrishikesh-3459@users.noreply.github.com>
2020-06-22 16:14:20 +0530

    Added morse to English

--- englishToMorse.py
+++ englishToMorse.py
@@ -1,6 +1,7 @@
 # Morse Code Translater
-def englishToMorse(s): # Function to convert English letters to Morse
-    MORSE_CODE_DICT = { 'A':'.-', 'B':'-...', 
+
+# A dictionary which maps letters to morse code
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...', 
                         'C':'-.-.', 'D':'-..', 'E':'.', 
                         'F':'..-.', 'G':'--.', 'H':'....', 
                         'I':'..', 'J':'.---', 'K':'-.-', 
@@ -14,23 +15,43 @@ def englishToMorse(s): # Function to convert English letters to Morse
                         '7':'--...', '8':'---..', '9':'----.', 
                         '0':'-----', ', ':'--..--', '.':'.-.-.-', 
                         '?':'..--..', '/':'-..-.', '-':'-....-', 
-                        '(':'-.--.', ')':'-.--.-'} 
+                        '(':'-.--.', ')':'-.--.-', "'" : '.----.'} 
+
+
+# Function to convert English letters to Morse
+def englishToMorse(s): 
     # Separating letter with spaces and words with tabs
     ans = []
     for i in s:
-        if i.upper() not in MORSE_CODE_DICT:
-            return -1
         if i == " ":
-            ans.append('\t') 
+            ans.append('\t')
+        elif i.upper() not in MORSE_CODE_DICT:
+            return -1
         else:
             ans.append(MORSE_CODE_DICT[i.upper()])
     letters = ' '.join(ans)
     return letters
 
+
+# Function to convert Morse to English
+def morseToEnglish(s):
+    eng_dict = {} # Creating a new dictionary, which maps morse code to letters
+    for i in MORSE_CODE_DICT:
+        eng_dict[MORSE_CODE_DICT[i]] = i
+    words = s.split('   ') # Separating letter with spaces and words with tabs
+    ans = []
+    for i in words:
+        letter = i.split()
+        for j in letter:
+            ans.append(eng_dict[j])
+        ans.append(' ')
+    return ''.join(ans)
+    
+
 # Error Handling for the choice
 while True:
     try:
-        choice = int(input("Enter\n1 for Morse to English\n2 for English to Morse\n"))
+        choice = int(input("1 for Morse to English\n2 for English to Morse\nEnter choice: "))
         if choice == 1 or choice == 2:
             break
         else:
@@ -38,14 +59,27 @@ while True:
     except ValueError:
         print("Oops, you must enter either 1 or 2")
 
+
+# Driver for Morse to English
+if choice == 1:
+    x = None
+    while True:
+        s = input("Please enter Morse Code: ").rstrip()
+        x = morseToEnglish(s)
+        if x != -1:
+            break
+        else:
+            print("Error!!!")
+    print(f"The Morse Code in English is : {x}" )
+
 # Driver for English to Morse
 if choice == 2:
     x = None
     while True:
-        s = input("Please enter the String: ")
+        s = input("Please enter the String: ").rstrip()
         x = englishToMorse(s)
         if x != -1:
             break
         else:
             print("Error!!!")
-    print(f"The Morse Code is : {x}" )
+    print(f"The Morse Code is : {x.strip()}" )
